8f096186586aa82a11d0a63a54bb180b01ed9ace
r3card0 <ricardo.rdz@pm.me>
2022-09-27 00:53:55 -0500

    Added submit button, connector and connection code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import mysql.connector as conn
+from config import config
 
 root = Tk()
 root.title('window Title')
@@ -10,6 +12,24 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 # entry data
+
+def connectdb():
+    params = config()
+    connection = conn.connect(**params)
+    
+    return connection
+
+cur = connectdb().cursor()
+
+connection = connectdb()
+
+def clear():
+    pass
+
+def submit():
+    pass
+
+# set label
 continent = StringVar()
 continent_entry = ttk.Entry(mainframe, width=20, textvariable=continent)
 continent_entry.grid(column=2, row=2, sticky=(W,E))
@@ -18,7 +38,8 @@ continent_label = ttk.Label(mainframe,text='Continent').grid(column=1, row=2,sti
 
 
 # submit button
-
+submit_button = Button(root, text="Submit", command=submit)
+submit_button.grid(row=2, column=0)
 
 # quit button
 frm = ttk.Frame(root, padding=10)
