40a9c687a7b1c6b09c078bfaea188cc6364a27a6
r3card0 <ricardo.rdz@pm.me>
2022-10-08 22:40:08 -0500

    tuple of continents

---
+++ scripts/continents_data.py
@@ -0,0 +1,11 @@
+def continents():
+    continents_tuple = ('America','Africa','Asia','Antartica','Ocenia','Europe')
+    return continents_tuple
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
